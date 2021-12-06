c5fbd23e2fc1bca4d532712cd1f24a1de05900cb
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2021-12-06 02:54:35 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'tailwindcss/tailwind.css'
-
+import '@material-tailwind/react/tailwind.css'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
