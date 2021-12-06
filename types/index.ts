d3bd844e68a39d6e47f9dfeba7d1cc59d665fcaa
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2021-12-06 10:40:45 +0200

    feat {Typescript}: Added some important types

---
+++ types/index.ts
@@ -0,0 +1,36 @@
+import { ParsedUrlQuery } from 'querystring'
+
+export interface User {
+  name: string
+  email: string
+  image: string
+  userId: string
+  role: string
+}
+export interface Post {
+  _id: string
+  title: string
+  subTitle: string
+  category: string
+  content: string
+  image: string
+  createdAt: string
+  writer: string
+}
+export interface Writer {
+  name: string
+  image: string
+}
+export interface GetPosts {
+  msg: string
+  posts: Post[]
+  length: number
+}
+export interface GetPost {
+  msg: string
+  post: Post
+  writer: Writer
+}
+export interface Id extends ParsedUrlQuery {
+  id: string
+}
