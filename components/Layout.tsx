a9109293e25411e311c0d967d4f0a5fb880da2a0
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2021-12-11 05:11:01 +0200

    google

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,8 +6,11 @@ const Layout: React.FC = ({ children }) => {
   return (
     <div className="bg-[#040203] text-gray-200 font-mkzy">
       <Head>
-        <title>Create Next App</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <meta
+          name="google-site-verification"
+          content="kO7ZgeZWyJC663DXNQu1mQ27oe3SThOTVlBNFnHFvSg"
+        />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Markazi+Text:wght@400;500;600;700&display=swap"
